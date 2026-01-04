4ee8e53880ce133128a97c27b3bbbd434b3ca6ff
manuellosadat2-a11y <manuellosadat2@gmail.com>
2026-01-04 22:39:48 +0000

    perfil condicional

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,77 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  // COVER (condicional)
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover === false) {
+    cover = `<div class="cover"></div>`;
+  }
+
+  // REDES SOCIALES (solo si existen)
+  let socialMediaHTML = "";
+
+  if (variables.twitter) {
+    socialMediaHTML += `
+      <li>
+        <a href="https://twitter.com/${variables.twitter}">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>`;
+  }
+
+  if (variables.github) {
+    socialMediaHTML += `
+      <li>
+        <a href="https://github.com/${variables.github}">
+          <i class="fab fa-github"></i>
+        </a>
+      </li>`;
+  }
+
+  if (variables.linkedin) {
+    socialMediaHTML += `
+      <li>
+        <a href="https://linkedin.com/in/${variables.linkedin}">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </li>`;
+  }
+
+  if (variables.instagram) {
+    socialMediaHTML += `
+      <li>
+        <a href="https://instagram.com/${variables.instagram}">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>`;
+  }
+
+  // HTML FINAL
+  document.querySelector("#widget_content").innerHTML = `
+    <div class="widget">
+      ${cover}
+
+      <img src="${variables.avatarURL}" class="photo" />
+
+      <h1>
+        ${variables.name || ""} ${variables.lastName || ""}
+      </h1>
+
+      ${variables.role ? `<h2>${variables.role}</h2>` : ""}
+
+      ${
+        variables.city || variables.country
+          ? `<h3>${variables.city || ""}${variables.city && variables.country ? ", " : ""}${variables.country || ""}</h3>`
+          : ""
+      }
+
+      <ul class="${variables.socialMediaPosition}">
+        ${socialMediaHTML}
+      </ul>
+    </div>
+  `;
 }
 
 /**
@@ -88,4 +138,4 @@ window.onload = function() {
       render(Object.assign(window.variables, values)); // render again the card with new values
     });
   });
-};
+}; 
